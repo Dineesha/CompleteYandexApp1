8b1d7e81cd6b3f582c2438363c5b552aa887437e
Dineesha <dinsuraw@gmail.com>
2016-05-16 17:36:50 +0530

    Change search servlet

--- src/main/java/loginmongo/SearchServlet.java
+++ src/main/java/loginmongo/SearchServlet.java
@@ -1,15 +1,17 @@
 package loginmongo;
 
-import com.mongodb.*;
 import org.apache.log4j.Logger;
-
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.UnknownHostException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -21,77 +23,61 @@ public class SearchServlet extends HttpServlet {
     final static Logger logger = Logger.getLogger(SearchServlet.class);
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String searchName = request.getParameter("form-searchname");
-        /*try {
-        Class.forName("com.mysql.jdbc.Driver");
+       // String searchName = request.getParameter("form-searchname");
+        PrintWriter out = response.getWriter();
+        String sql;
 
-        //creating connection with the database
-        Connection con= DriverManager.getConnection
-                ("jdbc:mysql://localhost:3306/login_form", "username","fname");
+        String params = request.getParameter("form-searchname");
 
+        sql = "select * from user where username LIKE \'%" + params + "%\';";
 
-            PreparedStatement ps=con.prepareStatement
-                    ("SELECT * FROM TranslateUsers WHERE firstname=searchName");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
-        MongoClient mongo = new MongoClient("localhost", 27017);
-        DB db = mongo.getDB("login_form");
-        DBCollection table = db.getCollection("test_user1");
+        JsonObject jsonObj;
+        JsonArray jsonArray = new JsonArray();
+        Connection con = null;
+        PreparedStatement st;
+        ResultSet rs;
+        ResultSet rs2;
+        //ResultSet rs3;
 
-        /**** Find and display ****/
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("fname", searchName);
-        //JSONArray userlist=new JSONArray();
+        try {
+            con = Database.cpds.getConnection();
+            st = con.prepareStatement(sql);
+            rs = st.executeQuery();
 
-        DBCursor cursor = table.find(searchQuery);
+            while (rs.next()) {
+                jsonObj = new JsonObject();
+                jsonObj.addProperty("username", rs.getString(1));
+                jsonObj.addProperty("fname", rs.getString(3));
+                jsonObj.addProperty("lname", rs.getString(4));
+                jsonObj.addProperty("dob", rs.getString(5));
+                jsonObj.addProperty("phone", rs.getString(6));
+                jsonObj.addProperty("country", rs.getString(7));
+                jsonObj.addProperty("email", rs.getString(9));
 
-        if (cursor.hasNext()) {
-            //response.sendRedirect("index.jsp?name=");
-            request.setAttribute("searchMessage", "1");
-            RequestDispatcher rd = request.getRequestDispatcher("/searchuser.jsp");
-            rd.include(request, response);
+                String citysql = "select city from city where city_id=" + Integer.parseInt(rs.getString(8)) + ";";
+                st = con.prepareStatement(citysql);
+                rs2 = st.executeQuery();
 
-
-
-           /* try {
-                Set<String> BasicDBObject = db.getCollectionNames();
-
-                for (String collectionName : BasicDBObject) {
-                    // System.out.println(collectionName);
-                    logger.info(collectionName);
+                while (rs2.next()) {
+                    jsonObj.addProperty("city", rs2.getString("city"));
                 }
-                // get a single collection
-                // System.out.println(table.toString());
-                logger.info("Get the result values" + table.toString());
-                //System.out.println("Done");
 
-            } catch (IOException e) {
-                String er1 = e.getMessage();
-                logger.error("Exception thrown  :\" , er1");
-                // logger.trace("exception :\" +er1");
-            }**/
 
-            }else if (searchName==null){
-            request.setAttribute("searchMessage", "2");
-            RequestDispatcher rd = request.getRequestDispatcher("/searchuser.jsp");
-            rd.include(request, response);
-
-        }
-        else{
-                request.setAttribute("errorMessage", "No results found !");
-                RequestDispatcher rd = request.getRequestDispatcher("/searchuser.jsp");
-                try {
-                    rd.include(request, response);
-                } catch (ServletException e) {
-                    e.printStackTrace();
-                    logger.error("Exception thrown  :\" ,e");
-                } catch (IOException e) {
-                    logger.error("Exception thrown  :\" , e");
-                }
+                jsonArray.add(jsonObj);
             }
+            out.println(jsonArray);
+
+        } catch (SQLException ex) {
+            logger.error("Error in username validate method..", ex);
+        } finally {
+            try {
+                logger.trace("Closing  connection..");
+                con.close();
+            } catch (SQLException e) {
+                logger.fatal("Error while closing connection..");
+                e.printStackTrace();
+            }
+        }
 
         }
 
